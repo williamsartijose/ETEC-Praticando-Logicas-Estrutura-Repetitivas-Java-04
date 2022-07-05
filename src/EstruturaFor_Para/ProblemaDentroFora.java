3ad0712382e115f375e8b60278bb6ed38ac29e1e
William Sarti Jose <william.jose@totvs.com.br>
2022-07-04 23:38:08 -0300

    Estrutura- for (PARA) - ProblemaDentroFora

---
+++ src/EstruturaFor_Para/ProblemaDentroFora.java
@@ -0,0 +1,42 @@
+package EstruturaFor_Para;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProblemaDentroFora {
+
+	public static void main(String[] args) {
+		// Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X
+		// que serão lidos em seguida.
+		// Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos
+		// estão fora do intervalo,
+		// conforme exemplo
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int n, x, fora, dentro;
+
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n = sc.nextInt();
+
+		fora = 0;
+		dentro = 0;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("Digite um numero: ");
+			x = sc.nextInt();
+
+			if (x < 10 || x > 20) {
+				fora++;
+			} else {
+				dentro++;
+			}
+		}
+
+		System.out.printf("%d DENTRO\n", dentro);
+		System.out.printf("%d FORA\n", fora);
+
+		sc.close();
+	}
+}
